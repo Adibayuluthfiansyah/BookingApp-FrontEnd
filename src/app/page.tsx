4da2c73da73254d5ebb9962bcccbeb51285e4f96
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-09-22 22:18:33 +0700

    fix bug at homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import React from "react";
-import Footer from "./utilities/footer/page";
 import Hero from "./hero/page";
-import CustomerHomePage from "./(customer)/homepage-cus/page";
 import VenuesPage from "./(customer)/venues/page";
 
 
