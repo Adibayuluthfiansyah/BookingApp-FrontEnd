4d3d4d50071835ac4d59073746351c8d8fbaf991
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-09-23 20:52:27 +0700

    fix pagehome VenueCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,24 @@
 import React from "react";
 import Hero from "./hero/page";
 import Footer from "./utilities/footer/page";
-
-
+import VenuesCard from "@/components/VenuesCard";
+import { MOCK_VENUES } from "@/lib/utils";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <div className="flex justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto py-16 px-6">
+      {MOCK_VENUES.map((venue) => (
+        <VenuesCard 
+          key={venue.id}
+          venue={venue} 
+          viewMode="grid"
+        />
+      ))}
+      </div>
+      </div>
       <Footer/>
     </div>
   );
